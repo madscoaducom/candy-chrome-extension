9ec711b34cbf7a93e14456bb3c88f5cc6342594a
Mads Flensted-Urech <mads@coadu.com>
2012-03-05 22:09:46 +0100

    Seperate out javascript

--- test.js
+++
@@ -1,5 +0,0 @@
-var u = document.getElementsByClassName('unread'); 
-for(sum = 0, i = 0; (e = u[i]) && i < u.length; i++) {
-  unread = isNaN(parseInt(e.innerHTML)) ? 0 : parseInt(e.innerHTML); sum += unread; console.log(i, sum);
-}; 
-console.log('UNREAD: ' + sum;
