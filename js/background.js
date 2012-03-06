436824e447fae78501882de1aa4ae8900632ec23
Mads Flensted-Urech <mads@coadu.com>
2012-03-06 22:34:34 +0100

    Rework the logic for handling chat tab
    - Discover already open chat tab on load
    - Show Active/In-active icon and badge text
    - Monitor removed event for closing the chat tab

--- js/background.js
+++ js/background.js
@@ -1,31 +1,62 @@
-function getTab(tabUrl, callback) {
-  chrome.tabs.getAllInWindow(undefined, function(tabs) {
-    for (var i = 0, tab; tab = tabs[i]; i++) {
-      if (tab.url && tab.url == tabUrl) {
-        callback(tab);
-      }
+var globalTabId = null;
+
+function showInactive() {
+  chrome.browserAction.setIcon({path: 'img/chat-grey.png'});
+  chrome.browserAction.setBadgeText({text: '?'});
+  chrome.browserAction.setBadgeBackgroundColor({color: [224,224,224,255]});
+}
+
+function showActive() {
+  chrome.browserAction.setIcon({path: 'img/chat.png'});
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
+}
+
+function getTab(tabUrl, foundCallback, notFoundCallback) {
+  chrome.tabs.query({'url': tabUrl}, function(tabs) {
+    if (tabs.length === 0) {
+      notFoundCallback();
     }
-    callback(null);
+    if (tabs.length === 1) {
+      foundCallback(tabs[0]);
+    }
+    if (tabs.length > 1) {
+      console.log('Found multiple open tabs');
+    }
   });
+}
+
+function injectContent(tabId){
+  chrome.tabs.executeScript(tabId, {file:'lib/jquery.min.js'});
+  chrome.tabs.executeScript(tabId, {file:'js/inject.js'});
 }
 
 function openChat() {
-  getTab(localStorage["chat_url"], function(tab) {
-    if (tab != null) {
-      chrome.tabs.update(tab.id, {selected: true});
-      return;
-    }
-    chrome.browserAction.setIcon({path:"img/chat.png"});
-    chrome.tabs.create({url: localStorage["chat_url"]}, function (tab) {
-      // Login, setting nickname from options
-      var chat_name = localStorage["chat_name"];
-      chrome.tabs.executeScript(tab.id, {code:"document.getElementById('username').value = '" + chat_name + "'; document.getElementsByTagName('input')[2].click();"});
-    });
+  showActive();
+  chrome.tabs.create({url: localStorage["chat_url"]}, function (tab) {
+    // Login, setting nickname from options
+    globalTabId = tab.id;
+    var chat_name = localStorage["chat_name"];
+    chrome.tabs.executeScript(tab.id, {code:"document.getElementById('username').value = '" + chat_name + "'; document.getElementsByTagName('input')[2].click();"});
+    injectContent(tab.id);
   });
 }
 
+function focusChat(tab) {
+  if (tab) {
+    chrome.tabs.update(tab.id, {selected: true});
+  }
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  openChat();
+   getTab(localStorage['chat_url'], focusChat, openChat);
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  if (tabId === globalTabId) {
+    globalTabId = null;
+    showInactive();
+  }
 });
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
@@ -38,3 +69,4 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   }
 });
 
+getTab(localStorage['chat_url'], function(tab){ globalTabId = tab.id; injectContent(tab.id); showActive(); }, function(){ showInactive(); globalTabId = null; });
