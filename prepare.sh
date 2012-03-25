6fe5c1ff69d1eccfe3d1ddea5202094ed66d6bc8
Mads Flensted-Urech <mads@coadu.com>
2012-03-25 23:01:43 +0200

    Capture test and browserify commands

---
+++ prepare.sh
@@ -0,0 +1,1 @@
+browserify lib/jquery.min.js lib/underscore-min.js js/scrape.js -o js/inject.js
---
+++ test.sh
@@ -0,0 +1,1 @@
+nodemon -w test -w test/lib -w src -x 'mocha --reporter min --ui tdd' test/scrape-test.js
